0c9ea91b094fc1cf645ade3ab20bec49f3da27b2
GochiusaLover <iorinxxxx@gmail.com>
2020-03-18 14:16:30 +0900

    added parse test

--- tests/TestParser.elm
+++ tests/TestParser.elm
@@ -15,24 +15,29 @@ ivar n =
 
 func = TypeFunction TypeBool TypeBool
 
-id =
+t ty =
+  if ty == TypeBool
+  then bvar 0
+  else ivar 0
+
+id ty =
   LambdaAbs
     "n"
-    TypeBool
-    (bvar 0)
+    ty
+    (t ty)
 
-app =
+app ty =
   LambdaApp
-    id
-    TermTrue
+    (id ty)
+    (if ty == TypeBool then TermTrue else TermInt 3)
 
-app2 =
+app2 ty =
   LambdaApp
     (LambdaApp
       ( LambdaAbs "n" func
         (LambdaVar 0 func)
       )
-      id
+      (id ty)
     )
     TermTrue
 
@@ -73,50 +78,90 @@ suite : Test
 suite =
   describe "t"
     [ describe "parse"
-      [
-        test "id"
-          <| \_ ->
-            ("\\n:Bool->n"
-              |> get
-            )
-          |> Expect.equal
-            (Success id "")
-      , test "app"
-          <| \_ ->
-            ("(\\n:Bool->n) true"
-              |> get
-            )
-          |> Expect.equal
-            (Success app "")
-      , test "not"
-          <| \_ ->
-            ("\\n:Bool->if n then false else true"
-              |> get
-            )
-          |> Expect.equal
-            (Success not "")
-      , test "app2"
-          <| \_ ->
-            ("(\\n:bool->bool->n)(\\n:bool->n)true"
-              |> get
-              |> (\res ->
-                  case res of
-                    Success hd "" -> evaluate Dict.empty hd
-                    _ -> LambdaVar -1 TypeBool
-                ))
-            |> Expect.equal TermTrue
-      , test "fail"
-          <| \_ ->
-            ("\\n->n"
-              |> get
-            )
-          |> Expect.equal Failed
+      [ describe "bool"
+        [
+          test "id"
+            <| \_ ->
+              ("\\n:Bool->n"
+                |> get
+              )
+            |> Expect.equal
+              (Success (id TypeBool) "")
+        , test "app"
+            <| \_ ->
+              ("(\\n:Bool->n) true"
+                |> get
+              )
+            |> Expect.equal
+              (Success (app TypeBool) "")
+        , test "not-abs"
+            <| \_ ->
+              ("\\n:Bool->if n then false else true"
+                |> get
+              )
+            |> Expect.equal
+              (Success not "")
+        , test "app2"
+            <| \_ ->
+              ("(\\n:bool->bool->n)(\\n:bool->n)true"
+                |> get
+                |> (\res ->
+                    case res of
+                      Success hd "" -> evaluate Dict.empty hd
+                      _ -> LambdaVar -1 TypeBool
+                  ))
+              |> Expect.equal TermTrue
+        , test "fail"
+            <| \_ ->
+              ("\\n->n"
+                |> get
+              )
+            |> Expect.equal Failed
+        ]
+      , describe "int"
+        [
+          test "id"
+            <| \_ ->
+              ("\\n:Int->n"
+                |> get
+              )
+            |> Expect.equal
+              (Success (id TypeInt) "")
+        , test "app"
+            <| \_ ->
+              ("(\\n:Int->n)3"
+                |> get
+              )
+            |> Expect.equal
+              (Success (app TypeInt) "")
+        , test "fail"
+            <| \_ ->
+              ("(\\n:Int->Int)"
+                |> get
+              )
+            |> Expect.equal Failed
+        ]
+      , describe "let"
+        [
+          test "not"
+            <| \_ ->
+              ("let not=\\n:bool->if n then false else true in not true"
+                |> get
+                |> (\res ->
+                    case res of
+                      Success hd "" -> evaluate Dict.empty hd
+                      _ -> LambdaVar -1 TypeBool
+                  ) 
+              )
+            |> Expect.equal TermFalse
+
+        ]
       ]
     , describe "toString"
       [
         test "id"
           <| \_ ->
-            ( id
+            ( (id TypeBool)
               |> (\exp -> termToString exp [])
             )
           |> Expect.equal "(\\ n ->n)"
